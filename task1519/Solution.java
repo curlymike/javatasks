5c10aa894bd2406298afc043d87ddefe257f485a
Mike <curlymike@gmail.com>
2018-01-06 17:01:13 +0300

    Добавил комментарии в решение задачи task1519

--- task1519/Solution.java
+++ task1519/Solution.java
@@ -16,27 +16,50 @@ public class Solution {
             boolean success = false;
             String line = cbr.readLine();
             if (line.equals("exit")) {
+                // Прерываем цикл while.
                 break;
             }
             if (line.indexOf('.') > -1) {
+                // В строке есть точка, попробуем получить Double.
                 try {
                     Double n = Double.parseDouble(line);
+                    // Если мы сдесь - это значит, что метод Double.parseDouble()
+                    // не выкинул исключение и у нас есть число.
                     success = true;
                     print(n);
-                } catch (NumberFormatException e) {}
+                } catch (NumberFormatException e) {
+                    // Хоть там и есть точка - это не число.
+                }
             }
             if (!success) {
+                // Или в строке нет точки или с Double ничего не получилось, попробуем Integer
                 try {
                     Integer n = Integer.parseInt(line);
+                    // Если мы сдесь - это значит, что метод Integer.parseInt()
+                    // не выкинул исключение и у нас есть число типа Integer.
                     success = true;
                     if (n > 0 && n < 128) {
+                        // Полученное число больше нуля и меньше 128
+                        // по условию задачи мы должны передать это 
+                        // число в виде переменной типа short.
+                        // Не важно, что тип short может принимать значения
+                        // от -32768 до 32767, просто такое условие, главное
+                        // что числа от 0 до 128 попадают в этот диапазон.
+                        // n - объект типа Integer, метод shortValue()
+                        // вернёт значение типа short.
                         print(n.shortValue());
                     } else {
+                        // Видимо число меньше либо равно нуля
+                        // или больше либо равно 128.
                         print(n);
                     }
-                } catch (NumberFormatException e) {}
+                } catch (NumberFormatException e) {
+                    // Нет, это не Integer
+                }
             }
             if (!success) {
+                // Попытки распознать в строке число не привели
+                // к успеху передаём параметром саму строку.
                 print(line);
             }
         }
